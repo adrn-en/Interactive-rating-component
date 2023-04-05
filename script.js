da726763fe4c6280b112b065ed03169309acffca
Adrian <126387264+adrn-en@users.noreply.github.com>
2023-04-06 00:11:19 +0800

    update js

--- script.js
+++ script.js
@@ -8,16 +8,11 @@ let rate = 0
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
-		const currentValue = button.value
 		buttons.forEach((btn) => {
-			const newValue = btn.value
-			if (newValue != currentValue) {
-				btn.classList.remove('focus')
-			} else {
-				btn.classList.add('focus')
-				rate = newValue
-			}
+			btn.classList.remove('focus')
 		})
+		button.classList.add('focus')
+		rate = button.value
 	})
 })
 
